94bbb88256160cf48acf6b24cbb6a709998feec6
Uzma K Rahman <uzma.rahman@gmail.com>
2016-12-06 09:22:00 -0800

    Only admins and project managers can edit a project's details

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,17 +1,19 @@
 require 'rails_helper'
 
-RSpec.feature "Users can edit existing projects" do
+RSpec.feature "Project managers can edit existing projects" do
   let(:user) { FactoryGirl.create(:user) }
   let(:project) { FactoryGirl.create(:project, name: "Sublime Text 3") }
   
   before do
-    assign_role!(user, :viewer, project)
-
     login_as(user)
+    assign_role!(user, :manager, project)
+
     visit "/"
     click_link "Sublime Text 3"
     click_link "Edit Project"
   end
+
+  
   scenario "with valid attributes" do
     fill_in "Name", with: "Sublime Text 4 beta"
     click_button "Update Project"
@@ -19,10 +21,12 @@ RSpec.feature "Users can edit existing projects" do
     expect(page).to have_content "Project has been updated."
     expect(page).to have_content "Sublime Text 4 beta"
   end 
+
   scenario "with invalid attributes" do
     fill_in "Name", with: ""
     click_button "Update Project"
 
     expect(page).to have_content "Project has not been updated."
   end
+  
 end
